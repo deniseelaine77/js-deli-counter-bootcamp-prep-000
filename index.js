e21d140ab2d437e2af3add7e61f1812c339f4a1c
dehast59@gmail.com <dehast59@gmail.com>
2018-09-07 15:20:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber(line, name){
   let i= line.length
   return("Welcome, " + name + ". You are number " + i + " in line.")
 }
+
+function nowService(line) {
+  if(line.length === 0, return "There is nobody waiting to be served!");
+  else 
+}
